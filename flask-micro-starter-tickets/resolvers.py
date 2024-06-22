601df54aa39019b294d9a6985a0dfeb799898962
devsimiyu <dev.wrodgerss@gmail.com>
2024-06-22 18:46:08 +0300

    Initial commit with tickets microservice setup

---
+++ flask-micro-starter-tickets/resolvers.py
@@ -0,0 +1,20 @@
+from ariadne import convert_kwargs_to_snake_case
+
+def get_tickets_list_resolver(obj, info):
+    return [{ 'id': '1234er', 'title': 'Hello ticket' }]
+
+@convert_kwargs_to_snake_case
+def get_tickets_details_resolver(obj, info, id):
+    return { 'id': '1234er', 'title': 'Hello ticket' }
+
+@convert_kwargs_to_snake_case
+def create_ticket_resolver(obj, info, title, description):
+    return { 'id': '1234er', 'title': title, 'description': description }
+
+@convert_kwargs_to_snake_case
+def update_ticket_resolver(obj, info, id, title, description):
+    return { 'id': id, 'title': title, 'description': description }
+
+@convert_kwargs_to_snake_case
+def delete_ticket_resolver(obj, info, id):
+    return 'Ticket will deleted'
